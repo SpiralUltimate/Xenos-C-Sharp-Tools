b74d7ae0adbcdd96ae15d3ca86f305c7fcf3043b
Coding <89676793+SpiralUltimate@users.noreply.github.com>
2023-12-30 10:57:25 -0800

    Started working on some basics including types and such.
    Just finished the basics of the extended function manager. This allows for a more game engine like coding experience.. and i created some flax like types like the float2 and the float3(WIP)

---
+++ Extensibility/Interfaces/BasicFunctionality.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensibilityPackage.ExtPackage.Extensibility.Interfaces
+{
+    public interface BasicFunctionality
+    {
+        public void OnStart();
+    }
+}
---
+++ Extensibility/Types.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensibilityPackage.ExtPackage.Extensibility
+{
+    public class Type2<T>
+    {
+        public readonly T one, two;
+        public Type2(T one, T two)
+        {
+            this.one = one;
+            this.two = two;
+        }
+        public TypeToGet[] GetValues<TypeToGet>(int OptMethod = (int)GetValueMethod.Normal)
+        {
+            TypeToGet[] Tarray = new TypeToGet[2];
+            TypeToGet _one, _two;
+            if (typeof(TypeToGet) == typeof(string))
+            {
+                if (OptMethod == (int)GetValueMethod.RemoveDot)
+                {
+                    if (one.ToString().Contains("."))
+                    {
+                        _one = (TypeToGet)(object)one.ToString().Remove(one.ToString().LastIndexOf('.')).Replace(".", "");
+                    }
+                    else
+                    {
+                        _one = (TypeToGet)(object)one.ToString();
+                    }
+                    if (two.ToString().Contains("."))
+                    {
+                        _two = (TypeToGet)(object)two.ToString().Remove(two.ToString().LastIndexOf('.')).Replace(".", "");
+                    }
+                    else
+                    {
+                        _two = (TypeToGet)(object)two.ToString();
+                    }
+                }
+                else
+                {
+                    _one = (TypeToGet)(object)one.ToString();
+                    _two = (TypeToGet)(object)two.ToString();
+                }
+            }
+            else if (typeof(TypeToGet) == typeof(int))
+            {
+                if (one.ToString().Contains("."))
+                {
+                    _one = (TypeToGet)(object)int.Parse(one.ToString().Remove(one.ToString().LastIndexOf('.')).Replace(".", ""));
+                }
+                else
+                {
+                    _one = (TypeToGet)(object)int.Parse(one.ToString());
+                }
+                if (two.ToString().Contains("."))
+                {
+                    _two = (TypeToGet)(object)int.Parse(two.ToString().Remove(two.ToString().LastIndexOf('.')).Replace(".", ""));
+                }
+                else
+                {
+                    _two = (TypeToGet)(object)int.Parse(two.ToString());
+                }
+            }
+            else
+            {
+                _one = (TypeToGet)(object)one;
+                _two = (TypeToGet)(object)one;
+            }
+            Tarray[0] = _one;
+            Tarray[1] = _two;
+            return Tarray;
+        }
+    }
+    public class Type3
+    {
+        public readonly float one, two, three;
+        public Type3(float one, float two, float three)
+        {
+            this.one = one;
+            this.two = two;
+            this.three = three;
+        }
+    }
+}
+enum GetValueMethod
+{
+    Normal = 0,
+    RemoveDot = 1,
+}
